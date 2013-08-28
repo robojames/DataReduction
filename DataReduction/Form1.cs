c60ab8f9c0c52d0622626f4576f674c552ebdf6f
James <JArmes@empiricaltesting.com>
2013-08-27 21:37:47 -0600

    Initial Commit

---
+++ DataReduction/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DataReduction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_CSV_1 = new OpenFileDialog();
+
+            open_CSV_1.DefaultExt = "|*.csv";
+
+            open_CSV_1.ShowDialog();
+
+            StreamReader CSV_Reader_1 = new StreamReader(open_CSV_1.FileName);
+
+            string line;
+            string[] temp_array;
+            int iter = 0;
+
+            List<double> Position_1 = new List<double>();
+            List<double> Load_1 = new List<double>();
+
+            while ((line = CSV_Reader_1.ReadLine()) != null)
+            {
+                if (iter > 3)
+                {
+                    temp_array = line.Split(',');
+
+                    Position_1.Add(Math.Abs(double.Parse(temp_array[4])));
+                    Load_1.Add(Math.Abs(double.Parse(temp_array[5])));
+                }
+
+                iter++;
+            }
+
+            // Massage the data
+            // first the position data
+            double Position_1_First = Position_1[0];
+            for (int i = 0; i < Position_1.Count; i++)
+            {
+                Position_1[i] = Position_1[i] - Position_1_First;
+            }
+
+            Plot plot_1 = new Plot();
+            plot_1.Show();
+            plot_1.Initialize_Plot(Position_1, Load_1);
+        }
+    }
+}
---
+++ DataReduction/Plot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataReduction
+{
+    public partial class Plot : Form
+    {
+        public Plot()
+        {
+            InitializeComponent();
+        }
+
+        private void Plot_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Initialize_Plot(List<double> X, List<double> Y)
+        {
+            chart1.Series.Add("Load vs. Displacement");
+
+
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            for (int i = 0; i < X.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(X[i], Y[i]);
+            }
+
+            chart1.Titles.Add("Static Axial Compression - 752028-F1");
+            chart1.Titles[0].Font = new Font("Garamond", 12.0f);
+
+            chart1.ChartAreas[0].AxisY.Minimum =  Y.Min();
+            chart1.ChartAreas[0].AxisY.Maximum =  Y.Max();
+            chart1.ChartAreas[0].AxisX.Minimum =  X.Min();
+            chart1.ChartAreas[0].AxisX.Maximum =  X.Max();
+
+            chart1.ChartAreas[0].AxisX.Interval = 0.10;
+            chart1.ChartAreas[0].AxisY.Interval = 2000;
+
+            chart1.ChartAreas[0].AxisY.LabelStyle.Format = "#";
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "#.##";
+
+            chart1.Series[0].IsVisibleInLegend = false;
+
+            chart1.ChartAreas[0].AxisX.Title = "Position, mm";
+            chart1.ChartAreas[0].AxisX.TitleFont = new Font("Garamond", 12.0f);
+            chart1.ChartAreas[0].AxisY.TitleFont = new Font("Garamond", 12.0f);
+            chart1.ChartAreas[0].AxisY.Title = "Load, N";
+
+            chart1.Show();
+        }
+
+        
+    }
+}
